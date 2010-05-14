aab72c5d9d35572a2b831cd0123498336ec16eeb
simonmcmanus <mcmanus.simon@gmail.com>
2010-05-14 05:05:18 -0700

    now using CKEditor 3.2 and auto checking out the tw_uploader CKEditor plugin.

--- tiddlydocs_static_files.sh
+++ tiddlydocs_static_files.sh
@@ -1,9 +1,12 @@
-mkdir static 
-cd static 
-mkdir ckeditor 
-wget -O ckeditor_3.0.tar.gz http://download.cksource.com/CKEditor/CKEditor/CKEditor%203.0/ckeditor_3.0.tar.gz
-tar xvf ckeditor_3.0.tar.gz
-rm ckeditor_3.0.tar.gz
- 
+mkdir static
+cd static
+mkdir ckeditor
+wget -O ckeditor_3.2.tar.gz http://download.cksource.com/CKEditor/CKEditor/CKEditor%203.2/ckeditor_3.2.tar.gz
+tar xvf ckeditor_3.2.tar.gz
+rm ckeditor_3.2.tar.gz
+cd ckeditor/plugins
+svn co http://svn.tiddlywiki.org/Trunk/contributors/SimonMcManus/tiddlyweb/CKEditor/tw_uploader/
+cd ../../
 svn co http://svn.tiddlywiki.org/Trunk/contributors/SimonMcManus/tiddlyweb/tiddlydocs/static/mypage_images/ mydocs_images
 svn co http://svn.tiddlywiki.org/Trunk/contributors/SimonMcManus/tiddlyweb/tiddlydocs/static/mypage_css/ mypage_css
+
